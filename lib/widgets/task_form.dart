4af7a99698447f7967cd6acaef682bba7a929059
khaled Ahmed <khaledahm79@gmail.com>
2024-11-06 00:30:01 +0200

    bottomshete

--- lib/widgets/task_form.dart
+++ lib/widgets/task_form.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import 'package:to_do/firebase/firebase_services.dart';
 import 'package:to_do/models/task_model.dart';
 import 'package:to_do/providers/task_provider.dart';
 import 'package:to_do/themes&colors/my_colors.dart';
@@ -24,85 +23,89 @@ class _TaskFormState extends State<TaskForm> {
   @override
   Widget build(BuildContext context) {
     var provider = Provider.of<TaskProvider>(context, listen: false);
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 25),
-      child: Form(
-        key: formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: Text(
-                "Add new task",
+    return Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 25),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  "Add new task",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+              MyTextField(
+                controller: titleController,
+                hint: "Title",
+                size: 20,
+                fontWeight: FontWeight.w700,
+                maxLines: 1,
+                validator: (p0) {
+                  if (p0!.isEmpty) {
+                    return "Enter title";
+                  }
+                  return null;
+                },
+              ),
+              MyTextField(
+                controller: descroptionController,
+                hint: "Description",
+                size: 18,
+                fontWeight: FontWeight.w400,
+                maxLines: 5,
+                validator: (p0) {
+                  return null;
+                },
+              ),
+              Text(
+                "Select the date:",
                 style: Theme.of(context).textTheme.titleMedium,
               ),
-            ),
-            MyTextField(
-              controller: titleController,
-              hint: "Title",
-              size: 20,
-              fontWeight: FontWeight.w700,
-              maxLines: 1,
-              validator: (p0) {
-                if (p0!.isEmpty) {
-                  return "Enter title";
-                }
-                return null;
-              },
-            ),
-            MyTextField(
-              controller: descroptionController,
-              hint: "Description",
-              size: 18,
-              fontWeight: FontWeight.w400,
-              maxLines: 5,
-              validator: (p0) {
-                return null;
-              },
-            ),
-            Text(
-              "Select the date:",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            TextButton(
-              onPressed: () async {
-                DateTime? now = await showDatePicker(
-                  context: context,
-                  firstDate: DateTime.now(),
-                  initialDate: currentDate,
-                  currentDate: DateTime.now(),
-                  lastDate: DateTime.now().add(const Duration(days: 365)),
-                );
-                setState(() {
-                  currentDate = DateTime(now!.year, now.month, now.day);
-                });
-              },
-              child: Text(
-                dateFormat.format(currentDate),
-                style: TextStyle(fontSize: 18, color: MyColors.lightBlue),
-              ),
-            ),
-            ElevatedButton(
-                style: IconButton.styleFrom(
-                    elevation: 10,
-                    foregroundColor: Colors.white,
-                    backgroundColor: MyColors.lightBlue,
-                    side: const BorderSide(color: Colors.white, width: 4)),
-                onPressed: () {
-                  if (formKey.currentState!.validate()) {
-                    provider.addTask(TaskModel(
-                        title: titleController.text.trim(),
-                        description: descroptionController.text.trim(),
-                        date: DateTime(currentDate.year, currentDate.month,
-                            currentDate.day)));
-                    Navigator.of(context).pop();
-                  }
+              TextButton(
+                onPressed: () async {
+                  DateTime? now = await showDatePicker(
+                    context: context,
+                    firstDate: DateTime.now(),
+                    initialDate: currentDate,
+                    currentDate: DateTime.now(),
+                    lastDate: DateTime.now().add(const Duration(days: 365)),
+                  );
+                  setState(() {
+                    currentDate = DateTime(now!.year, now.month, now.day);
+                  });
                 },
-                child: const Icon(
-                  Icons.done,
-                ))
-          ],
+                child: Text(
+                  dateFormat.format(currentDate),
+                  style: TextStyle(fontSize: 18, color: MyColors.lightBlue),
+                ),
+              ),
+              ElevatedButton(
+                  style: IconButton.styleFrom(
+                      elevation: 10,
+                      foregroundColor: Colors.white,
+                      backgroundColor: MyColors.lightBlue,
+                      side: const BorderSide(color: Colors.white, width: 4)),
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      provider.addTask(TaskModel(
+                          title: titleController.text.trim(),
+                          description: descroptionController.text.trim(),
+                          date: DateTime(currentDate.year, currentDate.month,
+                              currentDate.day)));
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  child: const Icon(
+                    Icons.done,
+                  ))
+            ],
+          ),
         ),
       ),
     );
